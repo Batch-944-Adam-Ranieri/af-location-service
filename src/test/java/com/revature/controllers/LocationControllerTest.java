51c5762b99f73def0d40f98255d3766b6cc6d4d1
samaurer2 <43677441+samaurer2@users.noreply.github.com>
2021-04-02 10:35:14 -0500

    Location Controller tests done

--- src/test/java/com/revature/controllers/LocationControllerTest.java
+++ src/test/java/com/revature/controllers/LocationControllerTest.java
@@ -2,10 +2,13 @@ package com.revature.controllers;
 
 import com.revature.AFLocationService.AfLocationServiceApplication;
 import com.revature.entities.Location;
+import com.revature.exceptions.LocationNotFoundException;
 import com.revature.services.LocationService;
+import com.sun.xml.bind.v2.TODO;
 import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +25,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -48,7 +52,7 @@ class LocationControllerTest {
 
     @Test
     void createLocationTest() throws Exception{
-        String json = "{locationId:12, city:test, state:test,zipcode:test}";
+        String json = "{locationId:0, city:test, state:test,zipcode:test}";
         mvc.perform(MockMvcRequestBuilders
                 .post("/locations")
                 .content(json)
@@ -60,95 +64,159 @@ class LocationControllerTest {
 
     @Test
     void getLocationByIdTest() throws Exception{
-        Location location = new Location(12, "testCity", "testState", "testZip");
+        Location location = new Location(12, "test","test","test");
         Mockito.when(locationService.getLocationById(12)).thenReturn(location);
-
-        ResultActions ra = mvc.perform(get("/locations/12"));
-        System.out.println(ra.andExpect(status().is(200)));
+        mvc.perform(MockMvcRequestBuilders
+                .get("/locations/12")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Authorized"))
+                .andExpect(status().isOk());
     }
 
     @Test
     void getAllLocationsTest() throws Exception {
         List<Location> locations = new ArrayList<>();
-        for (int i = 1; i < 5; ++i) {
-            Location location = new Location(i,"test", "test","test");
+        for(int i = 1; i <5; ++i) {
+            Location location = new Location(i,"test","test","test");
             locations.add(location);
         }
         Mockito.when(locationService.getAllLocations()).thenReturn(locations);
-        ResultActions ra = mvc.perform(get("/locations"));
-        ra.andExpect(status().is(200));
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/locations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Authorized"))
+                .andExpect(status().isOk());
     }
 
     @Test
     void updateLocationTest() throws Exception{
-        Location location = new Location(12, "testCity", "testState", "testZip");
-        Mockito.when(locationService.updateLocation(location)).thenReturn(location);
-
-        ResultActions ra = mvc.perform(put("/locations/12").header("Authorization", "authorized"));
-        ra.andExpect(status().is(HttpStatus.OK.value()));
+        Location location = new Location(12, "newtest", "test", "test");
+        Mockito.when(locationService.updateLocation(any(Location.class))).thenReturn(location);
+        String json = "{locationId:12, city:newtest, state:test,zipcode:test}";
+        mvc.perform(MockMvcRequestBuilders
+                .put("/locations/12")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Authorized"))
+                .andExpect(status().isOk());
     }
 
     @Test
     void deleteLocationTest() throws Exception{
         Mockito.when(locationService.deleteLocation(12)).thenReturn(true);
-        ResultActions ra = mvc.perform(delete("/locations/12").header("Authorization", "authorized"));
-        ra.andExpect(status().is(HttpStatus.OK.value()));
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/locations/12")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Authorized"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void createLocationTestForbidden() throws Exception{
+        String json = "{locationId:0, city:test, state:test,zipcode:test}";
+        mvc.perform(MockMvcRequestBuilders
+                .post("/locations")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Unauthorized"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void updateLocationTestForbidden() throws Exception{
+        String json = "{locationId:12, city:newtest, state:test,zipcode:test}";
+        mvc.perform(MockMvcRequestBuilders
+                .put("/locations/12")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Unauthorized"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void deleteLocationTestForbidden() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/locations/12")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Unauthorized"))
+                .andExpect(status().isForbidden());
     }
 
     @Test
     void createLocationTestUnauthorized() throws Exception{
-
-        ResultActions ra = mvc.perform(post("/locations").header("Authorization", "unauthorized"));
-        ra.andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));//is unauthorized
+        String json = "{locationId:0, city:test, state:test,zipcode:test}";
+        mvc.perform(MockMvcRequestBuilders
+                .post("/locations")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
     void updateLocationTestUnauthorized() throws Exception{
-        ResultActions ra = mvc.perform(put("/locations").header("Authorization", "unauthorized"));
-        ra.andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));//is unauthorized
+        String json = "{locationId:12, city:newtest, state:test,zipcode:test}";
+        mvc.perform(MockMvcRequestBuilders
+                .put("/locations/12")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
-    void deleteLocationTestUnauthorized() throws Exception{
-        ResultActions ra = mvc.perform(delete("/locations").header("Authorization", "unauthorized"));
-        ra.andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));//is unauthorized
-    }
-
-    @Test
-    void createLocationTestNoAuthorizedToken() throws Exception{
-        ResultActions ra = mvc.perform(post("/locations"));
-        ra.andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));//is unauthorized
-    }
-
-    @Test
-    void updateLocationTestNoAuthorizedToken() throws Exception{
-        ResultActions ra = mvc.perform(put("/locations/12"));
-        ra.andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));//is unauthorized
-    }
-
-    @Test
-    void deleteLocationTestNoAuthorizedToken() throws Exception{
-        ResultActions ra = mvc.perform(delete("/locations/12"));
-        ra.andExpect(status().is(HttpStatus.UNAUTHORIZED.value()));//is unauthorized
+    void deleteLocationTestNoUnauthorized() throws Exception{
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/locations/12")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
     void getLocationByIdTestDoesNotExist() throws Exception{
-        ResultActions ra = mvc.perform(get("/locations/9001"));
-        ra.andExpect(MockMvcResultMatchers.status().reason(StringContains.containsString("Cannot find location")));
+        Mockito.when(locationService.getLocationById(1000)).thenThrow(new LocationNotFoundException());
+        mvc.perform(MockMvcRequestBuilders
+                .get("/locations/1000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Authorized"))
+                .andExpect(status().reason("Location not found"));
     }
 
-    @Test
+    @Test //TODO add .equals() for location
+
     void updateLocationTestDoesNotExist() throws Exception{
-        ResultActions ra = mvc.perform(put("/locations/9001"));
-        //System.out.printf("ra status reason: %s\n", ra.andReturn());
-        ra.andExpect(MockMvcResultMatchers.status().reason(StringContains.containsString("Cannot find location")));
+
+        Location location = new Location(1000, "newtest", "test", "test");
+        String json = "{locationId:1000, city:newtest, state:test,zipcode:test}";
+        Mockito.when(locationService.updateLocation(location)).thenThrow(new LocationNotFoundException());
+        mvc.perform(MockMvcRequestBuilders
+                .put("/locations/1000")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Authorized"))
+                .andExpect(status().reason("Location not found"));
     }
 
     @Test
     void deleteLocationTestDoesNotExist() throws Exception{
-        ResultActions ra = mvc.perform(delete("/locations/9001"));
-        ra.andExpect(MockMvcResultMatchers.status().reason(StringContains.containsString("Cannot find location")));
+        Mockito.when(locationService.deleteLocation(1000)).thenThrow(new LocationNotFoundException());
+        mvc.perform(MockMvcRequestBuilders
+                .delete("/locations/1000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization","Authorized"))
+                .andExpect(status().isOk());
     }
 
 }
